30b1d2fb882307d9da6132865f5774dcfcb3e1db
Airi <airiagustus82@gmail.com>
2024-08-14 15:28:53 +0700

    First push

---
+++ app/src/main/java/com/learnjetpackcompose/temperatureconverterjetpackcompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.learnjetpackcompose.temperatureconverterjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.learnjetpackcompose.temperatureconverterjetpackcompose.ui.theme.TemperatureConverterJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TemperatureConverterJetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TemperatureConverterJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
